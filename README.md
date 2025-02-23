4d256c0fa75b26d6bd6378eeddb6f928a5ec7ca6
Eric Thornton <ewthornton@gmail.com>
2025-02-23 18:50:09 -0500

    add files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # IDF-WebCamServer
 ESP-IDF Version of the WebCamServer
+
+Taken from the arduino example, but converted to work with the espressif esp-idf framework.
---
+++ main/main.c
@@ -0,0 +1,190 @@
+#include <string.h>
+#include <esp_log.h>
+#include <esp_system.h>
+#include <nvs_flash.h>
+#include <sys/param.h>
+
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "esp_psram.h"
+
+#include "driver/gpio.h"
+#include "driver/ledc.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+
+// support IDF 5.x
+#ifndef portTICK_RATE_MS
+#define portTICK_RATE_MS portTICK_PERIOD_MS
+#endif
+
+#include "esp_camera.h"
+
+#define CAMERA_MODEL_AI_THINKER
+#include "camera_pins.h"
+#include "app_httpd.h"
+
+// Define WIFI_SSID and WIFI_PASS in this file!
+#include "secrets.h" 
+
+static const char *TAG = "MAIN";
+
+#define WIFI_CONNECTED_BIT BIT0
+
+/* Configure camera */
+static camera_config_t camera_config = 
+{
+    .ledc_channel = LEDC_CHANNEL_0,
+    .ledc_timer   = LEDC_TIMER_0,
+    .pin_pwdn = PWDN_GPIO_NUM,
+    .pin_reset = RESET_GPIO_NUM,
+    .pin_xclk = XCLK_GPIO_NUM,
+    .pin_sccb_sda = SIOD_GPIO_NUM,
+    .pin_sccb_scl = SIOC_GPIO_NUM,
+    .pin_d0 = Y2_GPIO_NUM,
+    .pin_d1 = Y3_GPIO_NUM,
+    .pin_d2 = Y4_GPIO_NUM,
+    .pin_d3 = Y5_GPIO_NUM,
+    .pin_d4 = Y6_GPIO_NUM,
+    .pin_d5 = Y7_GPIO_NUM,
+    .pin_d6 = Y8_GPIO_NUM,
+    .pin_d7 = Y9_GPIO_NUM,
+    .pin_vsync = VSYNC_GPIO_NUM,
+    .pin_href = HREF_GPIO_NUM,
+    .pin_pclk = PCLK_GPIO_NUM,
+    .xclk_freq_hz = 20000000,
+    .frame_size = FRAMESIZE_UXGA, //Set to maximum resolution by default so that buffer is maximized
+    .pixel_format = PIXFORMAT_JPEG,
+    //.grab_mode = CAMERA_GRAB_WHEN_EMPTY,
+    .grab_mode = CAMERA_GRAB_LATEST,
+    .fb_location = CAMERA_FB_IN_PSRAM,
+    .jpeg_quality = 12,
+    .fb_count = 2
+};
+
+
+static EventGroupHandle_t s_wifi_event_group;
+
+/* WiFi event handler */
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
+                               int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        esp_wifi_connect();
+        xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+        ESP_LOGI(TAG, "Retrying connection to the AP");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
+        ESP_LOGI(TAG, "Got IP: " IPSTR, IP2STR(&event->ip_info.ip));
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+/* Initialize WiFi in station mode */
+static void wifi_init_sta(void)
+{
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT,
+                                               ESP_EVENT_ANY_ID,
+                                               &wifi_event_handler,
+                                               NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT,
+                                               IP_EVENT_STA_GOT_IP,
+                                               &wifi_event_handler,
+                                               NULL));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+        },
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "WiFi initialization finished. Connecting...");
+
+    /* Wait for connection */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+                                           WIFI_CONNECTED_BIT,
+                                           pdFALSE,
+                                           pdTRUE,
+                                           portMAX_DELAY);
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "Connected to AP");
+    }
+}
+
+/* Set up LED flash using LEDC */
+void setupLedFlash(int pin)
+{
+    ledc_channel_config_t ledc_channel = {
+        .channel    = LEDC_CHANNEL_0,
+        .duty       = 0,
+        .gpio_num   = pin,
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .hpoint     = 0,
+        .timer_sel  = LEDC_TIMER_0
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+}
+
+static esp_err_t init_camera(void)
+{
+    //initialize the camera
+    esp_err_t err = esp_camera_init(&camera_config);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Camera Init Failed");
+        return err;
+    }
+
+    return ESP_OK;
+}
+
+
+void app_main(void)
+{
+    size_t psram_size = esp_psram_get_size();
+    ESP_LOGI(TAG, "PSRAM size: %i bytes", psram_size);
+
+    if(ESP_OK != init_camera()) {
+        ESP_LOGE(TAG, "Camera Init Failed");
+        return;
+    }
+
+    // NVS flash needed by WiFi
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES ||
+        err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+
+    /* Initialize WiFi */
+    wifi_init_sta();
+
+    // Let the things settle
+    vTaskDelay(1000 / portTICK_RATE_MS);
+
+    startCameraServer();
+
+    ESP_LOGI(TAG, "Camera Ready! Use 'http://<device_ip>' to connect");
+
+}
